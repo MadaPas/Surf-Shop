a636445f45a5c3006ba44ea1b818b59193ec1378
madapas <mada0193@stud.kea.dk>
2020-04-03 00:08:30 +0200

    Create the models (user, post, review)

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email:String,
+    //we dont need username and password but they are put into the schema by default
+    //cuz we use the passport local mongoose and it's all there 
+    image:String,
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
