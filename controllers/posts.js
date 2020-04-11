c8ba408052942a96bbb5669bcca680c32bc604cf
madapas <mada0193@stud.kea.dk>
2020-04-11 15:16:05 +0200

    Server-side upload & delete images

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,7 +22,7 @@ module.exports = {
     //Posts Create
     async postCreate(req, res, next) {
         req.body.post.images = [];
-        for(const file of req.files){
+        for (const file of req.files) {
             let image = await cloudinary.v2.uploader.upload(file.path);
             req.body.post.images.push({
                 url: image.secure_url,
@@ -55,10 +55,52 @@ module.exports = {
 
     //Posts update
     async postUpdate(req, res, next) {
-        let post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+        // find the post by id
+        //here we have access to the post that we want to do editing on 
+        let post = await Post.findById(req.params.id);
+        // check if there are any images for deletion
+        if (req.body.deleteImages && req.body.deleteImages.length) {
+            // assign deleteImages from req.body to its own variable
+            let deleteImages = req.body.deleteImages;
+            // loop over deleteImages
+            for (const public_id of deleteImages) {
+                // delete images from Cloudinary
+                await cloudinary.v2.uploader.destroy(public_id);
+                // delete an image from post.images
+                for (const image of post.images) {
+                    if (image.public_id === public_id) {
+                        let index = post.images.indexOf(image);
+                        post.images.splice(index, 1);
+                    }
+                }
+            }
+        }
+
+        // check if there are any new images for upload
+        if (req.files) {
+            // upload images
+            for (const file of req.files) {
+                let image = await cloudinary.v2.uploader.upload(file.path);
+                // add images to post .images array
+
+                post.images.push({
+                    url: image.secure_url,
+                    public_id: image.public_id
+                });
+            }
+
+        }
+        // update the post with any new properties
+        post.title = req.body.post.title;
+        post.description = req.body.post.description;
+        post.price = req.body.post.price;
+        post.location = req.body.post.location;
+
+        // save the updated post into the DB
+        post.save();
+
+        // redirect to show page
         res.redirect(`/posts/${post.id}`);
-        //handle any deletion of existing images
-        //handle upload of any new images 
     },
 
     //Posts delete
